d06d53b09b5095d911ea824846b4497e685d3956
Marshudi <eddiemarshudi@icloud.com>
2025-02-07 14:32:38 +0400

    Updated Image

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -56,21 +56,21 @@ const project_info = [
     pName: "World University Rankings",
     link: "https://github.com/marshudi/WorldUniversityRankings",
     figCap: "University Data Analysis",
-    image: "https://user-images.githubusercontent.com/76883519/271652355-7432f9c8-49a9-4ec3-b04f-d50d17137c4a.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3Mzg5MjI2NzgsIm5iZiI6MTczODkyMjM3OCwicGF0aCI6Ii83Njg4MzUxOS8yNzE2NTIzNTUtNzQzMmY5YzgtNDlhOS00ZWMzLWIwNGYtZDUwZDE3MTM3YzRhLnBuZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNTAyMDclMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjUwMjA3VDA5NTkzOFomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPTFiMTE4NzgyZWQ4OGZlNTVhZjk1ZTE1ODA3NWYyMzQ2ZjhiZGE2NWYwYmRkODk0NWRiNmI3MDk1MDBjOTU4NGYmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0In0.b48ovhgxN0uXJ7euO8dIQz52fzJglnDTEEssufKbjZ4",
+    image: "https://images.cnbctv18.com/wp-content/uploads/2022/06/QS-world-univerisities-1019x573.jpg",
     constant: "Click to Visit",
   },
   {
     pName: "Constraint Satisfaction Problem",
     link: "https://github.com/marshudi/Constraint-Satisfaction-Problem",
     figCap: "AI & Problem Solving",
-    image: "https://user-images.githubusercontent.com/76883519/410865661-521c2c9d-9c5f-4fe5-99da-612100552c7f.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3Mzg5MjMxOTksIm5iZiI6MTczODkyMjg5OSwicGF0aCI6Ii83Njg4MzUxOS80MTA4NjU2NjEtNTIxYzJjOWQtOWM1Zi00ZmU1LTk5ZGEtNjEyMTAwNTUyYzdmLnBuZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNTAyMDclMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjUwMjA3VDEwMDgxOVomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPWNmNzZjZGNiOWJlNzhhNGY3ZDE3NjRmOTM1YzNhM2Y0MGY3NDRkZGQyOTQ3ZTU3MzU5ZjVmMmVlNTZlOTQ2NGUmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0In0.JQW1xrlmyCPYjf8DCFlNWkNFKE5FC2qwerriShe_-Ws",
+    image: "https://www.researchgate.net/publication/2364273/figure/fig1/AS:668935657627659@1536498097509/An-example-of-a-graph-coloring-problem-and-its-representation.png",
     constant: "Click to Visit",
   },
   {
     pName: "Traffic Light Control System",
     link: "https://github.com/marshudi/Traffic-Light-Control-System",
     figCap: "Embedded Systems & IoT",
-    image: "https://user-images.githubusercontent.com/76883519/410866817-ef15a77b-624c-4941-a1af-18cb5215e6c3.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3Mzg5MjMzMTMsIm5iZiI6MTczODkyMzAxMywicGF0aCI6Ii83Njg4MzUxOS80MTA4NjY4MTctZWYxNWE3N2ItNjI0Yy00OTQxLWExYWYtMThjYjUyMTVlNmMzLnBuZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNTAyMDclMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjUwMjA3VDEwMTAxM1omWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPThkM2IxNWNmY2Y4MDJmYzBlMzRhYjgyNTY3ZmM0NjE2OTZhYjFiYzA1M2RkMDY1ODM4ZjU5Mzc1ODk0NTAwNTImWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0In0.E36MEwLVxvyIYVD4vAwuIqWmhX_uHbaszHsdJR7p7Zk",
+    image: "https://s1.cdn.autoevolution.com/images/news/gallery/how-traffic-light-control-systems-work_1.jpg",
     constant: "Click to Visit",
   },
   {
@@ -95,7 +95,7 @@ const Projects = () => {
       <div className="container">
         <ScrollAnimation animateIn="fadeIn" animateOut="fadeOut">
           <div className="row">
-            <div className="section-title">
+            <div className="section-title">E36MEwLVxvyIYVD4vAwuIqWmhX_uHbaszHsdJR7p7Zk
               <h2>
                 My <span>Projects</span>
               </h2>
