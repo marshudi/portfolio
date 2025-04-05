40e4c37bff4b7bb66c9f3de9d6382bee04dfd757
Mohammed Al-Marshudi <momarshudi@gmail.com>
2025-04-05 18:36:10 +0400

    NotFoundPage Added

--- src/App.js
+++ src/App.js
@@ -1,37 +1,37 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 
-import  Header from'./components/header'
-
-import  Skills from'./components/Skills'
-import  Projects from'./components/Projects'
-import  Footer from'./components/footer'
+import Header from './components/header';
+import Skills from './components/Skills';
+import Projects from './components/Projects';
+import Footer from './components/footer';
 import AboutMe from './components/aboutMe';
 import About from './components/about';
-//import AboutMe from './components/aboutMe';
-//import Projects from './components/Projects';
-
-
+import NotFound from './components/NotFound';
 
 function App() {
   return (
-<>
+    <Router>
+      <Header />
 
-  
-      <Header/>
+      <Switch>
+        {/* Main route */}
+        <Route exact path="/">
+          <>
+            <About />
+            <AboutMe />
+            <Skills />
+            <Projects />
+          </>
+        </Route>
 
-      <About/>
-      <AboutMe/>
-      <Skills/>
-      <Projects/>
-      <Footer/>
-  
+        {/* 404 route */}
+        <Route component={NotFound} />
+      </Switch>
 
-
-
-</>
-
-    
+      <Footer />
+    </Router>
   );
 }
 
